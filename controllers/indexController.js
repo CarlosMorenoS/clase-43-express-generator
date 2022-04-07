00feba5e4f3805c9227e9f519a38ddb7f4093998
carlosmoreno <carlosmorenosanabria77@gmail.com>
2022-04-06 22:45:28 -0300

    toyota

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const products = JSON.parse(fs.readFileSync (path.join(__dirname,'../data/productsDataBase.json'), "utf-8"))
+
+
 module.exports ={
     index: function(req, res, next) {
-        res.render('index', { title: 'Comision 13' });
+        res.render('index', { 
+          title: 'Comision 13' ,
+          products,
+        });
       }
 }
